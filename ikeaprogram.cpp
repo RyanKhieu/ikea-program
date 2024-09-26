da685e027ca4e7e6bf6a29e760b6c76ed1f5e4dc
Ryan Khieu <159482357+RyanKhieu@users.noreply.github.com>
2024-09-26 12:51:59 +0000

    added code from old codespace

---
+++ ikeaprogram.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int itemChoice = 0;
+bool run = true;
+
+int main() {
+    cout << "Hello, Welcome to IKEA Denville!" << endl;
+    while (run) {
+        cout << "What would you like to buy today? Enter the number of your selection. To stop shopping, enter 0." << endl;
+        cout << "1. Standing desks" << endl;
+        cout << "2. Black Curtains" << endl;
+        cout << "3. Tabletops" << endl;
+        cout << "4. Table legs" << endl;
+        cin >> itemChoice;
+        switch (itemChoice) {
+            case 0:
+                run = false;
+                break;
+            case 1:
+                cout << "Standing desks are on sale for $200!" << endl;
+                break;
+            case 2:
+                cout << "Black curtains are on sale for $20!" << endl;
+                break;
+            case 3:
+                cout << "Tabletops are on sale for $50!" << endl;
+                break;
+            case 4:
+                cout << "Table legs are on sale for $30!" << endl;
+                break;
+            default:
+                cout << "Invalid selection. Please try again." << endl;
+                break;
+        }
+    }
+}
