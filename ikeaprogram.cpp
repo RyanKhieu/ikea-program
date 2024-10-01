de3ea8d82fd212a8d436d40da9b538277a91c8ca
Ryan Khieu <159482357+RyanKhieu@users.noreply.github.com>
2024-09-30 21:42:53 -0400

    Finished basic item choices and added checkout

--- ikeaprogram.cpp
+++ ikeaprogram.cpp
@@ -1,5 +1,13 @@
+/**
+* Name: Neil Kumaran & Ryan Khieu
+* Program Name: IKEA Program
+* Date: 9/30/2024
+* Extra: Switch cases instead of if statements
+*/
+
 #include <iostream>
 using namespace std;
+int itemsOrdered = 0;
 int itemChoice = 0;
 int deskChoice = 0;
 double deskTotalPrice = 0;
@@ -7,19 +15,24 @@ int curtainChoice = 0;
 double curtainTotalPrice = 0;
 int tableChoice = 0;
 double tableTotalPrice = 0;
+int legChoice = 0;
+double legTotalPrice = 0;
 
 int main() {
     cout << "Hello, Welcome to IKEA Denville (Store No. 3008)!" << endl;
     while (true) {
         cout << "What would you like to buy today? Enter the number of your selection. To stop shopping, enter 0." << endl;
         cout << "1. Standing desks" << endl;
-        cout << "2. Black Curtains" << endl;
+        cout << "2. Blackout Curtains" << endl;
         cout << "3. Tabletops" << endl;
         cout << "4. Table legs" << endl;
         cin >> itemChoice;
         switch (itemChoice) {
             case 0:
-                cout << "Thank you for shopping at IKEA Denville! Your total is $" << deskTotalPrice + curtainTotalPrice + tableTotalPrice << endl;
+                cout << "Thank you for shopping at IKEA Denville! You ordered " << itemsOrdered << " items for a subtotal of" << deskTotalPrice + curtainTotalPrice + tableTotalPrice + legTotalPrice << endl;
+                cout << "NJ Sales Tax (6.625%): " << (deskTotalPrice + curtainTotalPrice + tableTotalPrice + legTotalPrice) * 0.06625 << endl;
+                cout << "Total: " << (deskTotalPrice + curtainTotalPrice + tableTotalPrice + legTotalPrice) * 1.06625 << endl;
+                cout << "Have a great day!" << endl;
                 return 0; // Exit the program
             case 1:
                 cout << "What size do you want your desk to be?" << endl;
@@ -30,57 +43,67 @@ int main() {
                 switch (deskChoice) {
                     case 1:
                         deskTotalPrice += 309.99;
+                        itemsOrdered++;
                         break;
                     case 2:
                         deskTotalPrice += 339.99;
+                        itemsOrdered++;
                         break;
                     case 3:
                         deskTotalPrice += 369.99;
+                        itemsOrdered++;
                         break;
                     default:
                         cout << "Invalid selection. Please try again." << endl;
                         break;
                 }
-
-2
                 break;
             case 2:
                 cout << "What length do you want your curtains to be?" << endl;
-                for (int i = 1; i <= 9; i++) {
-                cout << i << ". " << (44 + i) << "\", $" << (18.99 + i) << endl;
+                for (int i = 1; i <= 10; i++) {
+                    cout << i << ". " << (44 + i) << "\", $" << (18.99 + i) << endl;
                 }
-                cout << "What length do you want your curtains to be?" << endl;
                 cin >> curtainChoice;
                 switch (curtainChoice) {
                     case 1:
                         curtainTotalPrice += 19.99;
+                        itemsOrdered++;
                         break;
                     case 2:
                         curtainTotalPrice += 20.99;
+                        itemsOrdered++;
                         break;
                     case 3:
                         curtainTotalPrice += 21.99;
+                        itemsOrdered++;
                         break;
                     case 4:
                         curtainTotalPrice += 22.99;
+                        itemsOrdered++;
                         break;
                     case 5:
                         curtainTotalPrice += 23.99;
+                        itemsOrdered++;
                         break;
                     case 6:
                         curtainTotalPrice += 24.99;
+                        itemsOrdered++;
                         break;
                     case 7:
                         curtainTotalPrice += 25.99;
+                        itemsOrdered++;
                         break;
                     case 8:
                         curtainTotalPrice += 26.99;
+                        itemsOrdered++;
                         break;
                     case 9:
                         curtainTotalPrice += 27.99;
+                        itemsOrdered++;
                         break;
                     case 10:
                         curtainTotalPrice += 28.99;
+                        itemsOrdered++;
                         break;
                     default:
                         cout << "Invalid selection. Please try again." << endl;
@@ -93,21 +116,53 @@ int main() {
                 cout << "1. Wooden" << endl;
                 cout << "2. Marble" << endl;
                 cout << "3. Granite" << endl;
+                cin >> tableChoice;
                 switch (tableChoice) {
                     case 1:
-                        curtainTotalPrice += 29.99;
+                        tableTotalPrice += 29.99;
+                        itemsOrdered++;
                         break;
                     case 2:
-                        cur
-
+                        tableTotalPrice += 79.99;
+                        itemsOrdered++;
+                        break;
+                    case 3:
+                        tableTotalPrice += 99.99;
+                        itemsOrdered++;
+                        break;
+                    default:    
+                        cout << "Invalid selection. Please try again." << endl;
+                        break;
                 }
                 break;
             case 4:
-                cout << "Table legs are on sale for $30!" << endl;
+                cout << "What color table leg do you want?" << endl;
+                cout << "1. Black" << endl;
+                cout << "2. White" << endl;
+                cout << "3. Silver" << endl;
+                cin >> legChoice;
+                switch (legChoice) {
+                    case 1:
+                        legTotalPrice += 29.99;
+                        itemsOrdered++;
+                        break;
+                    case 2:
+                        legTotalPrice += 29.99;
+                        itemsOrdered++;
+                        break;
+                    case 3:
+                        legTotalPrice += 29.99;
+                        itemsOrdered++;
+                        break;
+                    default:
+                        cout << "Invalid selection. Please try again." << endl;
+                        break;
+                }
                 break;
             default:
                 cout << "Invalid selection. Please try again." << endl;
                 break;
+            
         }
     }
 }
